93935e1f40a9ac3fe1d9e2ab4956fa45159e3c22
Yogesh Sirsat <yogeshsirsat56@gmail.com>
2024-09-17 01:16:11 +0530

    Add api files in josn and yaml format.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { writeFileSync } from 'fs';
+import * as yaml from 'yaml'; // YAML conversion library
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
